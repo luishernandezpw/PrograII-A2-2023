fef33477f9ca655d57cc58a576f147be645e467f
Luis Hernandez <sarco_luis@hotmail.com>
2023-02-09 08:20:26 -0600

    Terminacion del conversor de monedas

--- app/src/main/java/com/ugb/miapp/MainActivity.java
+++ app/src/main/java/com/ugb/miapp/MainActivity.java
@@ -11,10 +11,42 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView temp;
+    Spinner spn;
+    conversores miConversor = new conversores();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btnConvertir);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temp = (TextView) findViewById(R.id.txtcantidad);
+                double cantidad = Double.parseDouble(temp.getText().toString());
+
+                spn = findViewById(R.id.spnde);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spna);
+                int a = spn.getSelectedItemPosition();
+
+                temp = findViewById(R.id.lblrespuesta);
+                temp.setText("Respuesta: "+ miConversor.convertir(0, de, a, cantidad));
+            }
+        });
+    }
+}
+class conversores{
+    double[][] valores = {
+            {1, 7.84, 24.63, 36.51, 581.78, 8.75, 0.93, 130.54, 82.52, 0.82},//monedas
+            {},//Longitud
+            {},//Peso
+            {},//Almacenamiento
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
