ef090e72920f664aaa4c43c6164b04604e346191
Luis Hernandez <sarco_luis@hotmail.com>
2023-03-15 10:28:32 -0600

    Se agrego el menu y si hicieron efectivas las opciones de agregar, modificar

--- app/src/main/java/com/ugb/miapp/MainActivity.java
+++ app/src/main/java/com/ugb/miapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.ugb.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,8 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.w3c.dom.Text;
 
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     String id="";
     Button btn;
     TextView temp;
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +34,34 @@ public class MainActivity extends AppCompatActivity {
                 guardar_agenda();
             }
         });
+        fab = findViewById(R.id.fabRegresarListaAmigos);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                regresarListaAmigos();
+            }
+        });
+        mostrar_datos_amigos();
+    }
+    void mostrar_datos_amigos(){
+        Bundle parametros = getIntent().getExtras();
+        accion = parametros.getString("accion");
+        if(accion.equals("modificar")){
+            String amigos[] = parametros.getStringArray("amigos");
+            id = amigos[0];
+
+            temp = findViewById(R.id.txtnombre);
+            temp.setText(amigos[1]);
+
+            temp = findViewById(R.id.txtdireccion);
+            temp.setText(amigos[2]);
+
+            temp = findViewById(R.id.txtTelefono);
+            temp.setText(amigos[3]);
+
+            temp = findViewById(R.id.txtemail);
+            temp.setText(amigos[4]);
+        }
     }
     void guardar_agenda(){
         try {
@@ -47,10 +79,18 @@ public class MainActivity extends AppCompatActivity {
 
             db_agenda = new BD(MainActivity.this, "",null,1);
             String result = db_agenda.administrar_agenda(id, nombre, direccion, telefono, email, accion);
-            String msg = result.equals("ok") ? "Registro guardado con exito." : result;
+            String msg = result;
+            if( result.equals("ok") ){
+                msg = "Registro guardado con exito";
+                regresarListaAmigos();
+            }
             Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
         }catch (Exception e){
             Toast.makeText(this, "Error en guardar agenda: "+ e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+    void regresarListaAmigos(){
+        Intent iListaAmigos = new Intent(MainActivity.this, lista_amigos.class);
+        startActivity(iListaAmigos);
     }
 }
--- app/src/main/java/com/ugb/miapp/lista_amigos.java
+++ app/src/main/java/com/ugb/miapp/lista_amigos.java
@@ -1,24 +1,89 @@
 package com.ugb.miapp;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class lista_amigos extends AppCompatActivity {
+    Bundle parametros = new Bundle();
     BD db_agenda;
     ListView lts;
     Cursor cAMigos;
+    FloatingActionButton btn;
     protected void onCreate(Bundle instance){
         super.onCreate(instance);
         setContentView(R.layout.lista_amigos);
         obtenerDatosAmigos();
+        btn = findViewById(R.id.btnAgregarAmigos);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parametros.putString("accion", "nuevo");
+                abrirAgregarAmigos(parametros);
+            }
+        });
     }
+    public void abrirAgregarAmigos(Bundle parametros){
+        Intent iAgregarAmigos = new Intent(lista_amigos.this, MainActivity.class);
+        iAgregarAmigos.putExtras(parametros);
+        startActivity(iAgregarAmigos);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mimenu, menu);
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        cAMigos.moveToPosition(info.position);
+        menu.setHeaderTitle(cAMigos.getString(1)); //1=> Nombre del amigo...
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        try{
+            switch (item.getItemId()){
+                case R.id.mnxAgregar:
+                    parametros.putString("accion", "nuevo");
+                    abrirAgregarAmigos(parametros);
+                    return true;
+                case R.id.mnxModificar:
+                    String amigos[] = {
+                        cAMigos.getString(0), //idAmigo
+                        cAMigos.getString(1), //nombre
+                        cAMigos.getString(2), //direccion
+                        cAMigos.getString(3), //telefono
+                        cAMigos.getString(4), //email
+                    };
+                    parametros.putString("accion", "modificar");
+                    parametros.putStringArray("amigos", amigos);
+                    abrirAgregarAmigos(parametros);
+                    return true;
+                default:
+                    return super.onContextItemSelected(item);
+            }
+        }catch (Exception e){
+            return super.onContextItemSelected(item);
+        }
+    }
+
     public void obtenerDatosAmigos(){
         try {
             db_agenda = new BD(lista_amigos.this, "", null, 1);
