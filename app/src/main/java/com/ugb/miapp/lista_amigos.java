55ce251e83f96b1a82a6c870349b28bc2f499af8
Luis Hernandez <sarco_luis@hotmail.com>
2023-03-16 08:46:41 -0600

    Se agrego el metodo de eliminar y el cuadro de dialogopara confirmar, ademas se comenso a crear la busqueda de amigos

--- app/src/main/java/com/ugb/miapp/lista_amigos.java
+++ app/src/main/java/com/ugb/miapp/lista_amigos.java
@@ -1,8 +1,11 @@
 package com.ugb.miapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,9 +14,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -26,10 +31,13 @@ public class lista_amigos extends AppCompatActivity {
     ListView lts;
     Cursor cAMigos;
     FloatingActionButton btn;
+    final ArrayList<String> alAmigos = new ArrayList<String>();
+    final ArrayList<String> alAmigosCopy = new ArrayList<String>();
     protected void onCreate(Bundle instance){
         super.onCreate(instance);
         setContentView(R.layout.lista_amigos);
         obtenerDatosAmigos();
+        buscarAmigos();
         btn = findViewById(R.id.btnAgregarAmigos);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +84,9 @@ public class lista_amigos extends AppCompatActivity {
                     parametros.putStringArray("amigos", amigos);
                     abrirAgregarAmigos(parametros);
                     return true;
+                case R.id.mnxEliminar:
+                    eliminarDatosAmigos();
+                    return true;
                 default:
                     return super.onContextItemSelected(item);
             }
@@ -83,14 +94,36 @@ public class lista_amigos extends AppCompatActivity {
             return super.onContextItemSelected(item);
         }
     }
-
+    void eliminarDatosAmigos(){
+        try{
+            AlertDialog.Builder confirmacion = new AlertDialog.Builder(lista_amigos.this);
+            confirmacion.setTitle("Esta seguro de eliminar a: ");
+            confirmacion.setMessage(cAMigos.getString(1));
+            confirmacion.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    db_agenda.administrar_agenda(cAMigos.getString(0), "", "", "", "","eliminar");
+                    obtenerDatosAmigos();
+                    dialogInterface.dismiss();
+                }
+            });
+            confirmacion.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            confirmacion.create().show();
+        }catch (Exception e){
+            Toast.makeText(this, "Error al eliminar: "+ e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
     public void obtenerDatosAmigos(){
         try {
             db_agenda = new BD(lista_amigos.this, "", null, 1);
             cAMigos = db_agenda.consultar_agenda();
             if(cAMigos.moveToFirst()){
                 lts = findViewById(R.id.ltsAmigos);
-                final ArrayList<String> alAmigos = new ArrayList<String>();
                 final ArrayAdapter<String> adAmigos = new ArrayAdapter<String>(lista_amigos.this,
                         android.R.layout.simple_expandable_list_item_1, alAmigos);
                 lts.setAdapter(adAmigos);
@@ -105,5 +138,46 @@ public class lista_amigos extends AppCompatActivity {
         }catch (Exception e){
             Toast.makeText(this, "Error al obtener amigos: "+ e.getMessage(), Toast.LENGTH_LONG).show();
         }
+    }
+    void buscarAmigos(){
+        TextView temp = findViewById(R.id.txtBuscarAmigos);
+        temp.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try{
+                    alAmigos.clear();
+                    String valor = temp.getText().toString().trim().toLowerCase();
+                    if( valor.length()<=0 ){//es porque no esta escribiendo mostramos
+                        // la lista completa de amigos
+                        alAmigos.addAll(alAmigosCopy);
+                    }else{ //si esta buscando amigos...
+                        /*for(amigos amigo : alAmigosCopy){
+                            String idAmigo = amigo.getidAmigo();
+                            String nombre = amigo.getNombre();
+                            String direccion = amigo.getDireccion();
+                            String telefono = amigo.getTelefono();
+                            String email = amigo.getEmail();
+
+                            if( nombre.toLowerCase().trim().contains(valor) ||
+                                direccion.toLowerCase().trim().contains(valor) ||
+                                telefono.toLowerCase().trim().contains(valor) ||
+                                email.toLowerCase().trim().contains(valor)){
+                                alAmigos.add(amigo);
+                            }
+                        }*/
+                    }
+                }catch (Exception e){
+                    Toast.makeText(lista_amigos.this, "Error al buscar amigos: "+ e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
