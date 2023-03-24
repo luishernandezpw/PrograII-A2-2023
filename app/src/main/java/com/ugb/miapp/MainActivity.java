2e9c9ef7a89f101987bb64fce0c3da52908f227f
Luis Hernandez <sarco_luis@hotmail.com>
2023-03-24 10:48:20 -0600

    Se agrego el apartado de fotos a agregar amigos

--- app/src/main/java/com/ugb/miapp/MainActivity.java
+++ app/src/main/java/com/ugb/miapp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.ugb.miapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,6 +23,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     BD db_agenda;
     String accion="nuevo";
@@ -22,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView temp;
     FloatingActionButton fab;
+    ImageView img;
+    String urlCompletaImg="";
+    Intent tomarFotoIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mostrar_datos_amigos();
+        img = findViewById(R.id.imgAmigo);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFotoAmigo();
+            }
+        });
     }
     void mostrar_datos_amigos(){
         Bundle parametros = getIntent().getExtras();
@@ -92,5 +114,47 @@ public class MainActivity extends AppCompatActivity {
     void regresarListaAmigos(){
         Intent iListaAmigos = new Intent(MainActivity.this, lista_amigos.class);
         startActivity(iListaAmigos);
+    }
+    private void tomarFotoAmigo(){
+        tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if( tomarFotoIntent.resolveActivity(getPackageManager())!=null ){
+            File fotoAmigo = null;
+            try{
+                fotoAmigo = crearImagenAmigo();
+                if( fotoAmigo!=null ){
+                    Uri uriFotoAmigo = FileProvider.getUriForFile(MainActivity.this,
+                            "com.ugb.miapp.fileprovider", fotoAmigo);
+                    tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoAmigo);
+                    startActivityForResult(tomarFotoIntent, 1);
+                }
+            }catch (Exception ex){
+                Toast.makeText(this, "Error al tomar la foto: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if( resultCode==1 && resultCode==RESULT_OK ){
+                Bitmap imagenBitmap = BitmapFactory.decodeFile(urlCompletaImg);
+                img.setImageBitmap(imagenBitmap);
+            }
+        }catch (Exception ex){
+            Toast.makeText(this, "Error al mostrar la camara: "+ ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File crearImagenAmigo() throws Exception{
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "imagen_"+ fechaHoraMs +"_";
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if(dirAlmacenamiento.exists()==false ){
+            dirAlmacenamiento.mkdirs();
+        }
+        File image = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
+        urlCompletaImg = image.getAbsolutePath();
+        return image;
     }
 }
